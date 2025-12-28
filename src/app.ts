b10f47604ebc198a3a3d2c709118086d63eb8b58
manik-babu <mdmanikbabu48@gmail.com>
2025-12-28 23:08:33 +0600

    Add better-auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
+import cors from 'cors';
 
 const app: Application = express();
 
-// better auth
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded());
+// better auth
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
 
