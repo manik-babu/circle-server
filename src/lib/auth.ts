2c8652b467ccd0981834db0738db9150f483d522
manik-babu <mdmanikbabu48@gmail.com>
2025-12-29 14:59:28 +0600

    Add signup functionality with email and pass

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-import nodemailer from 'nodemailer';
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // Use true for port 465, false for port 587
-    auth: {
-        user: "rsmanik437@gmail.com",
-        pass: "2B 0R N0T 2B",
-    },
-});
+import sendMail from "../helper/sendEmail";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -29,18 +19,18 @@ export const auth = betterAuth({
     },
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
     },
     emailVerification: {
+        sendOnSignUp: true,
         sendVerificationEmail: async ({ user, url, token }, request) => {
-            const info = await transporter.sendMail({
-                from: '"Blogger babu" <blog@blog.com>',
-                to: "rsmanik99@gmail.com",
-                subject: "Hello, Manik",
-                text: "Hello world?", // Plain-text version of the message
-                html: "<b>Hello world?</b>", // HTML version of the message
-            });
-
-            console.log("Message sent:", info.messageId);
+            try {
+                await sendMail(user, url, token);
+            }
+            catch (error) {
+                console.log("An error accured ", error);
+            }
         },
     },
 });
