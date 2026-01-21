22be991fc42d1c647371c3987c3fd22146b8dc4f
manik-babu <mdmanikbabu48@gmail.com>
2026-01-21 17:20:07 +0600

    Fix like response

--- src/modules/like/like.controller.ts
+++ src/modules/like/like.controller.ts
@@ -6,7 +6,11 @@ const like = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const result = await likeService.like(req.params.postId!, req.user?.id!);
 
-        res.status(200).json(result);
+        res.status(200).json({
+            ok: true,
+            message: result,
+            data: null
+        });
     } catch (error: any) {
         next(error);
     }
@@ -16,6 +20,7 @@ const getLikedPeople = async (req: Request, res: Response, next: NextFunction) =
         const result = await likeService.getLikedPeople(req.params.postId!);
 
         res.status(200).json({
+            ok: true,
             message: "Liked people retrived successfully",
             data: result
         })
